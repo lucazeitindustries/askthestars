38a83c16ab4955187aef8d87980bef5e3b85f41f
Luca Ringwald <luca@zeitindustries.com>
2026-03-13 10:06:34 -0700

    fix: remove hardcoded Stripe API version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = process.env.STRIPE_SECRET_KEY
-  ? new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2025-02-24.acacia' as Stripe.LatestApiVersion })
+  ? new Stripe(process.env.STRIPE_SECRET_KEY)
   : null;
 
 export const PLANS = {
